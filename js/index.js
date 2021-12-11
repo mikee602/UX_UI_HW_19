2a4e630e37f3a0142492408abb9bf0bfd44c03e2
mikee602 <85657352+mikee602@users.noreply.github.com>
2021-12-10 20:36:08 -0600

    Typewriter effect

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 console.log("Hello world!");
- 
+
+var app = document.getElementById('headline');
+var typewriter = new Typewriter(app, {
+    loop: true,
+    cursor: "",
+});
+typewriter.typeString('Hello!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('I am Michael Everett')
+    .pauseFor(2500)
+    .start();
+
